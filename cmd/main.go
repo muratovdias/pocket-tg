e1a785eaaf2f36f1c009dd6e824495959a2dda41
muratovdias <muratovdias1601@gmail.com>
2023-05-30 21:16:40 +0600

    wrote basic functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/muratovdias/pocket-tg/pkg/telegram"
+	"log"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("6250725689:AAFD5w19F5mE3PE6EZUrD5leeeg8qTnVF_I")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.NewBot(bot)
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -1,1 +1,57 @@
 package telegram
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+)
+
+type Bot struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewBot(bot *tgbotapi.BotAPI) *Bot {
+	return &Bot{bot: bot}
+}
+func (b *Bot) Start() error {
+	log.Printf("Authorized on account %s", b.bot.Self.UserName)
+
+	updates := b.initUpdatesChannel()
+
+	if err := b.handleUpdates(updates); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) error {
+	for update := range updates {
+		if update.Message == nil { // If we got a message
+			continue
+		}
+		if err := b.handleMessage(update.Message); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
+	log.Printf("[%s] %s", message.From.UserName, message.Text)
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
+
+	// Send the message.
+	if _, err := b.bot.Send(msg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Bot) initUpdatesChannel() tgbotapi.UpdatesChannel {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := b.bot.GetUpdatesChan(u)
+
+	return updates
+}
